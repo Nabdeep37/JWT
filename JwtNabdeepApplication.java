9e02c519ffa31172ba2682bbf3eebcf6d157345c
Nabdeep37 <123183458+Nabdeep37@users.noreply.github.com>
2024-07-04 08:06:01 -0700

    Add files via upload

---
+++ JwtNabdeepApplication.java
@@ -0,0 +1,13 @@
+package com.example.JWT_nabdeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtNabdeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtNabdeepApplication.class, args);
+	}
+
+}
